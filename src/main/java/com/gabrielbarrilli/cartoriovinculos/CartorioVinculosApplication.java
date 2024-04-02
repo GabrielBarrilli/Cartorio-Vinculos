828426b4b4bfee73ab94a1b28dfcea51b7762672
Gabriel Barrilli <gabrielbarrillicruz@gmail.com>
2024-04-02 20:17:47 -0300

    Base do projeto criado

---
+++ src/main/java/com/gabrielbarrilli/cartoriovinculos/CartorioVinculosApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielbarrilli.cartoriovinculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartorioVinculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartorioVinculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielbarrilli/cartoriovinculos/CartorioVinculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielbarrilli.cartoriovinculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartorioVinculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
