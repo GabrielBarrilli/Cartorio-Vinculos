f3d44a5d3f92e8c2a752f2dc35579ecc70f2bf10
Gabriel Barrilli <gabrielbarrillicruz@gmail.com>
2024-04-16 20:02:34 -0300

    Configuração do swagger implementada

---
+++ src/main/java/com/gabrielbarrilli/cartoriovinculos/configuration/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.gabrielbarrilli.cartoriovinculos.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("security", securityScheme()))
+                .info(
+                        new Info()
+                                .title("REST API - PESSOA VINCULO")
+                                .description("API para gestão de pessoas e seus vínculos")
+                                .version("v1")
+                                .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                                .contact(new Contact().name("Gabriel").email("gabriel@gmail.com"))
+                );
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .description("Insira um bearer token válido para prosseguir")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("security");
+    }
+}
